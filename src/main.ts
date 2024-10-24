4b2ff09fcd89c164dbf0e003b6fd11674bfa1262
RomanLesovoy <romadesire97@gmail.com>
2024-10-24 11:46:25 +0300

    feat: cors

--- src/main.ts
+++ src/main.ts
@@ -3,6 +3,13 @@ import { AppModule } from './app.module';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
-  await app.listen(3000);
+
+  app.enableCors({
+    origin: process.env.ORIGIN,
+    methods: 'GET, POST, PUT, DELETE',
+    allowedHeaders: 'Content-Type, Authorization',
+  });
+
+  await app.listen(process.env.PORT);
 }
 bootstrap();
